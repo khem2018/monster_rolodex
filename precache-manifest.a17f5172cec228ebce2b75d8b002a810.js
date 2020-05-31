e8fdc4d8923bbf5d4b45801733d54b3dc4aac84e
khem <kdsharma075@gmail.com>
2020-05-31 15:29:22 +0530

    Updates

---
+++ precache-manifest.a17f5172cec228ebce2b75d8b002a810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412f385120bbf6431bf47003f61ae72c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5feb3c46e73c44544fee",
+    "url": "/monster-rolodex/static/css/main.d75cb8f8.chunk.css"
+  },
+  {
+    "revision": "cecf738d584200afd7be",
+    "url": "/monster-rolodex/static/js/2.c8a84cf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.c8a84cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5feb3c46e73c44544fee",
+    "url": "/monster-rolodex/static/js/main.849b516a.chunk.js"
+  },
+  {
+    "revision": "8db0b7d716aafbe7d0f4",
+    "url": "/monster-rolodex/static/js/runtime-main.5ceeb50c.js"
+  }
+]);
